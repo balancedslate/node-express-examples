54d7f45820791b57696f47f742e7425a8e566a89
Levi <83174877+balancedslate@users.noreply.github.com>
2022-09-22 17:36:24 -0400

    Added Express upload

---
+++ NodeExamples/index.js
@@ -0,0 +1,8 @@
+var superhero = require("superheroes");
+var supervillains = require("supervillains");
+
+var myHeroName = superhero.random();
+var myVillainName = supervillains.random();
+
+console.log(myHeroName);
+console.log(myVillainName);
